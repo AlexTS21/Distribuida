df8d75077ad9ef752b4db8e78d87992956b8e9bb
Alejandro Tejeda <alejanafs28@gmail.com>
2025-09-24 22:47:47 -0600

    Se implemento la segunda slucion de lista ligada y se creo un metodo de addValue que convina ambas soluciones

--- Hash/hash.py
+++ Hash/hash.py
@@ -1,3 +1,5 @@
+from linkedList import LinkedList
+
 def hashFunctionSuma(value, size):
     return sum(ord(char) for char in value)%size
 
@@ -31,29 +33,69 @@ class HashTable:
         self.table = [Row() for _ in range(size)]
         self.hashFunction = hashFuntion
     
-
+    def addInLinkedList(self, key, value):
+        hashKey = self.hashFunction(key, self.size)
+        if self.table[hashKey].flag == 2:
+            #Significa que la lista ya se creo entonces se agrega el dato al final
+            self.record[key] = [value, self.table[hashKey].value.size]
+            self.table[hashKey].value.insert(value)
+            
+        else:
+            self.table[hashKey].flag = 2
+            ant = self.table[hashKey].value
+            self.table[hashKey].value = LinkedList()
+            self.table[hashKey].value.insert(ant)
+            #Incertar todos los valores que estaban dentro de la tabla
+            print(self.record)
+            for k, v in self.record.items():
+                if hashKey == self.hashFunction(k, self.size):
+                    print(k, v)
+                    if v[1] != 0:
+                        plusIndex = self.record[k][1]
+                        self.table[plusIndex + hashKey].value = None
+                        self.table[plusIndex + hashKey].flag = None
+                        v[1] = self.table[hashKey].value.size
+                        self.table[hashKey].value.insert(v[0])
+            self.record[key] = [value,  self.table[hashKey].value.size]
+            self.table[hashKey].value.insert(value)
+            
+            
     def addInNextRow(self, key, value):
         #Basicamente la flag en 1 significa que es posible que el valor que esta ahi no sea el que apunta la funcion hash
         hashKey = self.hashFunction(key, self.size)
-        if self.table[hashKey].value == None:
-            self.table[hashKey].value = value
-            self.table[hashKey].flag = 0
-            #Agregar al historial 
-            self.record[key] = [value, 0]
-        else:
-            self.table[hashKey].flag = 1
-            i = hashKey
-            #Revisar la proxima casilla vacia y guardar indice
-            while i != (hashKey-1) % self.size:
-                if self.table[i].value == None:
-                    break
-                i = (i + 1) % self.size # avanzar circular
-            self.table[i].value = value
-            self.table[i].flag = 1
-            #Agregar al historial 
-            self.record[key] = [value, i - hashKey]
-            
+        if not self.isFull():
+            if self.table[hashKey].value == None:
+                self.table[hashKey].value = value
+                self.table[hashKey].flag = 0
+                #Agregar al historial 
+                self.record[key] = [value, 0]
+                return True
+            else:
+                self.table[hashKey].flag = 1
+                i = hashKey
+                #Revisar la proxima casilla vacia y guardar indice
+                while i != (hashKey-1) % self.size:
+                    if self.table[i].value == None:
+                        break
+                    i = (i + 1) % self.size # avanzar circular
+                self.table[i].value = value
+                self.table[i].flag = 1
+                #Agregar al historial 
+                self.record[key] = [value, i - hashKey]
+                return True
+        return False
 
+    def isFull(self):
+        for row in self.table:
+            if row.flag == None:
+                return False
+        return True
+
+    def addValue(self, key, value):
+        #Si el metodo de incercion al siguiente espacio falla entonces hacer lista ligada
+        if not self.addInNextRow(key, value):
+            self.addInLinkedList(key, value)
+       
     def getValue(self, key):
         hashKey = self.hashFunction(key, self.size)
         if self.table[hashKey].flag == 0:
@@ -61,24 +103,31 @@ class HashTable:
         elif self.table[hashKey].flag == 1:
             #Revisar el historial
             plusIndex = self.record[key][1]
-            
             return self.table[plusIndex + hashKey].value
-        return "No encontre el valor ):"
+        elif self.table[hashKey].flag == 2:
+            listIndex = self.record[key][1]
+            return self.table[hashKey].value.getValue(listIndex)
+        return "Valor no encontrado"
 
     def printTable(self):
-        print(f"i -> value  |   flag")
+        print(f"i -> falg  |   value")
         for i, t in enumerate(self.table):
-            print(f"{i} -> {t.value}    |   {t.flag}")
+            print(f"{i} -> {t.flag}    |   {t.value}")
 
 
 
 #la filosofia es agregar cuantos elementos posibles a la tabla hash con el metodo uno (Incercion al siguiente)
-hash_Table = HashTable(4, hashFunctionSuma)
-hash_Table.addInNextRow("omar", 10)
-hash_Table.addInNextRow("maro", 12)
-hash_Table.addInNextRow("ramo", 13)
+hash_Table = HashTable(3, hashFunctionSuma)
+
+hash_Table.addValue("omar", 10)
+hash_Table.addValue("maro", 12)
+hash_Table.addValue("ramo", 13)
 hash_Table.printTable()
 
-print(hash_Table.getValue("ramo"))
+hash_Table.addValue("mora", 14)
+hash_Table.printTable()
+hash_Table.addValue("jesu", 20)
+hash_Table.printTable()
+
 print(hash_Table.getValue("omar"))
-print(hash_Table.getValue("maro"))
+print(hash_Table.getValue("morA"))
--- Hash/linkedList.py
+++ Hash/linkedList.py
@@ -1,3 +1,39 @@
+from node import Node
+
 class LinkedList:
     def __init__(self):
-        pass
+        self.head = None
+        self.size = 0
+
+    def insert(self, valor):
+        """Inserta un nodo al final de la lista."""
+        new = Node(valor)
+        if not self.head:
+            self.head = new
+        else:
+            actual = self.head
+            while actual.next:
+                actual = actual.next
+            actual.next = new
+        self.size += 1
+
+    def getValue(self, indice):
+        """Devuelve el valor en el índice dado (0-based), sin eliminarlo."""
+        if indice < 0 or indice >= self.size:
+            raise IndexError("Índice fuera de rango")
+
+        actual = self.head
+        for _ in range(indice):
+            actual = actual.next
+        return actual.value
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        valores = []
+        actual = self.head
+        while actual:
+            valores.append(str(actual.value))
+            actual = actual.next
+        return " -> ".join(valores) if valores else "Lista vacía"
