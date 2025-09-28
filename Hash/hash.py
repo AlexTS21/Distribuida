d664420d159f1048315e3c3e045a1f0f947c36a4
Alejandro Tejeda <alejanafs28@gmail.com>
2025-09-27 21:06:51 -0600

    Se corrgieron erroes de incercion y logica al combinar las dos soluciones

--- Hash/hash.py
+++ Hash/hash.py
@@ -10,7 +10,7 @@ def hashFunctionMultiply(value, size):
     return suma%size
 
 class Row:
-    def __init__(self, value=None, flag=None):
+    def __init__(self, value=None, key=None, module=None, flag=None):
         """
         Clase que representa un renglon en la tabla hash
         value -> variable: contenido de la tabla hash
@@ -18,8 +18,14 @@ class Row:
         2  si existio colision y se aplico la solucion 2
         """
         self.value = value
+        self.key = key
+        self.module = module
         self.flag = flag
         return
+
+    def __str__(self):
+        return f"{self.value}-{self.key}-{self.module}-{self.flag}"
+    
 
 class HashTable:
     def __init__(self, size, hashFuntion):
@@ -33,56 +39,69 @@ class HashTable:
         self.table = [Row() for _ in range(size)]
         self.hashFunction = hashFuntion
     
+
+    def moveValues(self, module):
+        #Funcion que mueve todos los valores con modulo a el indice modulo en una lista ligada
+        self.table[module].value = LinkedList()
+        self.table[module].flag = 2
+        #Los valores se incertan en orden por lo que podemos recorrer el diccionario sin ningun problema
+        for k, v in self.record.items():
+            if v[1] == module:
+                v[2] = self.table[module].value.size
+                self.table[module].value.insert(Row(v[0], k, module))
+        for row in self.table:
+            if row.flag != 2 and row.module == module:
+                row.value, row.flag, row.key, row.module = None, None, None, None
+        return
+
+
     def addInLinkedList(self, key, value):
         hashKey = self.hashFunction(key, self.size)
         if self.table[hashKey].flag == 2:
-            #Significa que la lista ya se creo entonces se agrega el dato al final
-            self.record[key] = [value, self.table[hashKey].value.size]
-            self.table[hashKey].value.insert(value)
-            return f"key: {key} -> hashKey: {hashKey} se incerta en la lista ligada del indice {hashKey} en posicion {self.table[hashKey].value.size-1}"
+            #Lista ligada ya generada, incertar en ultima posicion
+            self.record[key] = [value, hashKey, self.table[hashKey].value.size]
+            self.table[hashKey].value.insert(Row(value, key, hashKey))
+            return f"El elemento se agrego en el indice {self.table[hashKey].value.size} de la lista ligada en {hashKey} (tabla)"
         else:
-            self.table[hashKey].flag = 2
-            ant = self.table[hashKey].value
-            self.table[hashKey].value = LinkedList()
-            self.table[hashKey].value.insert(ant)
-            #Incertar todos los valores que estaban dentro de la tabla
-            for k, v in self.record.items():
-                if hashKey == self.hashFunction(k, self.size):
-                    if v[1] != 0:
-                        plusIndex = self.record[k][1]
-                        self.table[plusIndex + hashKey].value = None
-                        self.table[plusIndex + hashKey].flag = None
-                        v[1] = self.table[hashKey].value.size
-                        self.table[hashKey].value.insert(v[0])
-            self.record[key] = [value,  self.table[hashKey].value.size]
-            self.table[hashKey].value.insert(value)
-            return f"key: {key} -> hashKey: {hashKey} Tabla llena, se crea lista ligada en posicion {hashKey} todos los elementos con hashKey: {hashKey} se incertan en la lista ligada"
+            if hashKey == self.table[hashKey].module:
+                self.moveValues(hashKey)
+                message = self.addInLinkedList(key, value)
+                return f"Los datos con key {hashKey} se agregaron a una lista ligada en {hashKey}. {message}"
+            else:
+                newMod = self.table[hashKey].module
+                self.moveValues(newMod)
+                message = self.addInNextRow(key, value)
+                return f"Se libero el espacio en {hashKey} con una lista ligada en {newMod}. {message}"                
+
+            
             
             
     def addInNextRow(self, key, value):
-        #Basicamente la flag en 1 significa que es posible que el valor que esta ahi no sea el que apunta la funcion hash
         hashKey = self.hashFunction(key, self.size)
         if not self.isFull():
             if self.table[hashKey].value == None:
                 self.table[hashKey].value = value
+                self.table[hashKey].key = key
+                self.table[hashKey].module = hashKey
                 self.table[hashKey].flag = 0
-                #Agregar al historial 
-                self.record[key] = [value, 0]
-                return f"key: {key} -> hashKey: {hashKey} sin colision se inserta en {hashKey}"
-            elif self.table[hashKey].flag != 2 and self.table[hashKey].value != None:
+                self.record[key] = [value, hashKey, hashKey]
+                return f"Sin colision, {key} se inserto posicion {hashKey}"
+            else:
                 self.table[hashKey].flag = 1
-                i = hashKey
                 #Revisar la proxima casilla vacia y guardar indice
+                i = hashKey
                 while i != (hashKey-1) % self.size:
                     if self.table[i].value == None:
                         break
                     i = (i + 1) % self.size # avanzar circular
                 self.table[i].value = value
+                self.table[i].key = key
+                self.table[i].module = hashKey
                 self.table[i].flag = 1
-                #Agregar al historial 
-                self.record[key] = [value, i - hashKey]
-                return f"key: {key} -> hashKey: {hashKey} colision en {hashKey} incerta en siguiente casilla disponible {i}"
-        return False
+                self.record[key] = [value, hashKey, i]
+                return f"Con colision, {key} se inserto posicion vacia proxima {i}"
+        return 
+
 
     def isFull(self):
         for row in self.table:
@@ -93,9 +112,9 @@ class HashTable:
     def addValue(self, key, value):
         #Si el metodo de incercion al siguiente espacio falla entonces hacer lista ligada
         message = self.addInNextRow(key, value)
-        print(message)
         if not message:
             message = self.addInLinkedList(key, value)
+        print(message)
         return message
     
     def getValue(self, key):
@@ -104,17 +123,17 @@ class HashTable:
             return self.table[hashKey].value
         elif self.table[hashKey].flag == 1:
             #Revisar el historial
-            plusIndex = self.record[key][1]
-            return self.table[plusIndex + hashKey].value
+            i = self.record[key][2]
+            return self.table[i].value
         elif self.table[hashKey].flag == 2:
             listIndex = self.record[key][1]
             return self.table[hashKey].value.getValue(listIndex)
         return False
 
     def printTable(self):
-        print(f"i -> falg  |   value")
+        print(f"i -> falg  |   value    | module    |   key")
         for i, t in enumerate(self.table):
-            print(f"{i} -> {t.flag}    |   {t.value}")
+            print(f"{i} -> {t.flag}    |   {t.value}    |   {t.module}  |   {t.key}")
 
 
 
@@ -124,13 +143,17 @@ hash_Table = HashTable(3, hashFunctionSuma)
 hash_Table.addValue("omar", 10)
 hash_Table.addValue("maro", 12)
 hash_Table.addValue("ramo", 13)
-hash_Table.printTable()
-print(hash_Table.getValue("ramo"))
-
-hash_Table.addValue("mora", 14)
-hash_Table.printTable()
-hash_Table.addValue("jesu", 20)
+hash_Table.addValue("roma", 17)
 hash_Table.printTable()
 
-print(hash_Table.getValue("omar"))
-print(hash_Table.getValue("morA"))
+hash_Table.addValue("hola", 14)
+hash_Table.addValue("hoal", 15)
+#
+hash_Table.addValue("ohja", 16)
+
+hash_Table.printTable()
+#hash_Table.addValue("jesu", 20)
+#hash_Table.printTable()
+#
+#print(hash_Table.getValue("omar"))
+#print(hash_Table.getValue("morA"))
