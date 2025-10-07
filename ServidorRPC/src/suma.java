d8033281511668128d08f3c845327b43bc54fe7e
wronskiano183 <146133857+wronskiano183@users.noreply.github.com>
2025-10-06 18:31:06 -0600

    version2

---
+++ ServidorRPC/src/suma.java
@@ -0,0 +1,15 @@
+public class suma {
+    public int sumar(int a, int b) {
+        System.out.println("Llamada para sumar dos datos (" + a + ", " + b + ")");
+
+        // servidor ocupado
+        try {
+            Thread.sleep(8000); // 8 segundos 
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Suma completada: " + a + " + " + b + " = " + (a + b));
+        return a + b;
+    }
+}
