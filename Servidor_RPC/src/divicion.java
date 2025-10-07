db293a17e26354d06549dcfa16f5e89a1e114f05
joctan-maceda <joctanfail69858@gmail.com>
2025-10-07 09:36:01 -0600

    Se eliminó duplicado

--- Servidor_RPC/src/divicion.java
+++
@@ -1,7 +0,0 @@
-
-public class divicion {
-     public int divicion(int a, int b){
-      System.out.println("Llamada para dividir dos datos (" + a + ", " + b + ")");  
-      return a / b;
-    }
-}
--- Servidor_RPC/src/resta.java
+++
@@ -1,7 +0,0 @@
-
-public class resta {
-      public int resta(int a, int b) {
-        System.out.println("Llamada para restar dos datos (" + a + ", " + b + ")");
-        return a - b;
-    }
-}
